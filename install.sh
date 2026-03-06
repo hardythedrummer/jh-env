074c9552d456292d51c93e563efc674fd8aacf74
Joey Hardy <joey.hardy@taillight.com>
2026-03-06 15:51:18 -0600

    updated starship config, add vscode settings to install script

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ link() {
 link "$DOTFILES/zsh/.zshrc"             "$HOME/.zshrc"
 link "$DOTFILES/wezterm/wezterm.lua"    "$HOME/.wezterm.lua"
 link "$DOTFILES/starship/starship.toml" "$HOME/.config/starship.toml"
+link "$DOTFILES/vscode/settings.json"    "$HOME/Library/Application Support/Code/User/settings.json"
 link "$DOTFILES/git/.gitconfig"          "$HOME/.gitconfig"
 link "$DOTFILES/git/.gitconfig-personal" "$HOME/.gitconfig-personal"
 link "$DOTFILES/git/.gitconfig-work"     "$HOME/.gitconfig-work"
