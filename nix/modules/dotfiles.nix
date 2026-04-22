2e15ad9790155391d4eed0229fde2359507755de
Joey Hardy <joey.hardy@taillight.com>
2026-04-22 16:14:25 -0500

    remove claude settings from dotfiles

--- nix/modules/dotfiles.nix
+++ nix/modules/dotfiles.nix
@@ -6,10 +6,5 @@
   home.file = {
     ".wezterm.lua".source = ../../wezterm/wezterm.lua;
     "Library/Application Support/Code/User/settings.json".source = ../../vscode/settings.json;
-    ".claude/settings.json".source = ../../claude/settings.json;
-    ".claude/get-key.sh" = {
-      source = ../../get-key.sh;
-      executable = true;
-    };
   };
 }
